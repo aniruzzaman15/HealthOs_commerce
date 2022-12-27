1c6f4fbb3645fcefed4e7c7b8455bfa316a96965
Aniruzzaman anir <107474981+uihut-anir@users.noreply.github.com>
2022-12-28 00:06:06 +0600

    tailwind added

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  //preflight reset every html elements css in default mode
+
+  // corePlugins: {
+  //   preflight: false,
+  // }
+}
